c3d0b615f9ea68ec36e41a90489ddda05408ffdb
JOOWON <wer07032@naver.com>
2023-06-19 14:45:37 +0900

    Initial commit

---
+++ src/main/java/com/dali186/queuing/QueuingApplication.java
@@ -0,0 +1,13 @@
+package com.dali186.queuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dali186/queuing/QueuingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dali186.queuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
