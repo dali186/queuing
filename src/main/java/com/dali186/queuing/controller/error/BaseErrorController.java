4909904707e38d6d4fa3a75eb2a1958edea3f971
JOOWON <wer07032@naver.com>
2023-06-21 15:59:09 +0900

    Link ErrorPage - #7
    ErrorPage 연동

---
+++ src/main/java/com/dali186/queuing/controller/error/BaseErrorController.java
@@ -0,0 +1,29 @@
+package com.dali186.queuing.controller.error;
+
+import com.dali186.queuing.constant.ErrorCode;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class BaseErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ModelAndView error(HttpServletResponse response) {
+        HttpStatus status = HttpStatus.valueOf(response.getStatus());
+        ErrorCode errorCode = status.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(status.getReasonPhrase())
+                        ), status);
+    }
+}
